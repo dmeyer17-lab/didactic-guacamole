e968b387ffc188c1f48675ac3bd359deb85d2ab2
dmeyer17-lab <dmeyer@pugetsound.edu>
2025-02-06 07:47:34 +0000

    final submission

--- heap.c
+++ heap.c
@@ -1,8 +1,9 @@
 /*
  * heapsort.c
  *
- *  Created on: Jul 1, 2013
- *      Author:
+ *  Created on: Jul 1, 2013, 2/5/2025
+ *
+ *      Author: Devan Meyer
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,12 +18,14 @@
  */
 void heapSort(Employee *A, int n)
 {
-	// TODO - BuildHeap on the heap
-
-	// TODO - while n > 0:
-	// TODO - swap A[n-1] with A[0], since A[0] is the smallest number.
-	// TODO - A[n-1] now sorted in place, so decrement n
-	// TODO - Heapify the elements from A[0] up to A[n-1] (which leaves the newly sorted element alone)
+	// BuildHeap on the heap
+	buildHeap(A, n);
+	// while n > 0:
+	for (int i = n - 1; i > 0; i--)
+	{
+		swap(&A[0], &A[i]); // swap A[n-1] with A[0], since A[0] is the smallest number.
+		heapify(A, 0, i);	// Heapify the elements from A[0] up to A[n-1] (which leaves the newly sorted element alone)
+	}
 }
 
 /**
@@ -35,7 +38,11 @@ void heapSort(Employee *A, int n)
  */
 void buildHeap(Employee *A, int n)
 {
-	// TODO - heapify() every element from A[n/2] down-to A[0]
+	// heapify() every element from A[n/2] down-to A[0]
+	for (int i = n / 2 - 1; i >= 0; i--)
+	{
+		heapify(A, i, n);
+	}
 }
 
 /**
@@ -48,15 +55,46 @@ void buildHeap(Employee *A, int n)
  */
 void heapify(Employee *A, int i, int n)
 {
-	// TODO - get index of left child of element i
-	// TODO - get index of right child of element i
+	//	get index of left child of element i
+	int left_child = 2 * (i + 1) - 1;
 
-	// TODO - determine which child has a smaller salary. We'll call the index of this
-	//		element: "smaller"
+	//	get index of right child of element i
 
-	// TODO - recursively check if the salary at A[i] > the salary at A[smaller]. If it is, swap the two.
-	//			Then recursively heapify A[smaller].
-	// TODO - Continue recursion as long as i is within range AND either right_child and left_child are still within range.
+	int right_child = 2 * (i + 1);
+
+	//	determine which child has a smaller salary. We'll call the index of this
+	//	element: "smaller"
+
+	int smaller = i;
+
+	printf("heapifying: i=%d, left_child=%d, right_child=%d, n=%d\n", i, left_child, right_child, n); // show heapify values for error checking
+
+	if (left_child < n && A[left_child].salary < A[smaller].salary)
+	{
+		smaller = left_child;
+	}
+
+	if (right_child < n && A[right_child].salary < A[smaller].salary)
+	{
+		smaller = right_child;
+	}
+
+	//	recursively check if the salary at A[i] > the salary at A[smaller]. If it is, swap the two.
+	//	Then recursively heapify A[smaller].
+
+	if (A[i].salary > A[smaller].salary && smaller != i)
+	{
+		if (smaller >= n || i >= n)
+		{
+			printf("Error: Attempting to swap out-of-bounds index! i=%d, smaller=%d, n=%d\n", i, smaller, n);
+			return; // Prevents seg fault
+		}
+
+		printf("Swapping index %d (%d) with index %d (%d)\n", i, A[i].salary, smaller, A[smaller].salary); // show swap values for error checking
+
+		swap(&A[i], &A[smaller]);
+		heapify(A, smaller, n);
+	}
 }
 
 /**
@@ -66,7 +104,11 @@ void heapify(Employee *A, int i, int n)
  */
 void swap(Employee *e1, Employee *e2)
 {
-	// TODO
+	if (!e1 || !e2)
+		return;			 // Prevent null pointer errors
+	Employee temp = *e1; //	Create temp to save index of e1 after first swap
+	*e1 = *e2;			 //	e1 gets e2's index
+	*e2 = temp;			 //	e2 gets e1's index
 }
 
 /**
@@ -76,5 +118,15 @@ void swap(Employee *e1, Employee *e2)
  */
 void printList(Employee *A, int n)
 {
-	// TODO
+	printf("[");
+	for (int i = 0; i < n; i++)
+	{
+		printf("id: %s, sal: %d", A[i].name, A[i].salary);
+
+		if (i < n - 1)
+		{
+			printf("], [");
+		}
+	}
+	printf("]\n");
 }
--- main.c
+++ main.c
@@ -1,10 +1,41 @@
+/*
+ * main.c
+ *
+ *  	Uploaded 2/5/2025
+ *
+ *      Author: Devan Meyer
+ */
 #include <stdio.h>
 #include "employee.h"
 #include "heap.h"
 
-int main(int argc, char *argv[])
+#define MAX_EMPLOYEES 5
+
+/**
+ * Driver function for heap.c, takes user input to create an array of preset size customizable above via MAX_EMPLOYEES.
+ * Assumes that user will input non-decimal numbers.
+ */
+int main()
 {
-	//TODO
-	
+	Employee employees[MAX_EMPLOYEES];
+	int cmdLine = 1; // value to # cmdlines
+
+	for (int i = 0; i < MAX_EMPLOYEES; i++)
+	{
+		printf("%d Name: ", cmdLine);
+		cmdLine++;
+		scanf("%49s", employees[i].name); //	Scan for input string and prevent buffer overflow
+		printf("%d Salary: ", cmdLine++);
+		scanf("%d", &employees[i].salary); // Scan for user int input for salary
+		printf("%d\n", cmdLine++);
+	}
+	printf("%d ", cmdLine++);
+
+	heapSort(employees, MAX_EMPLOYEES); // Sort input array
+
+	printf("\n%d ", cmdLine++);
+
+	printList(employees, MAX_EMPLOYEES); // Print out sorted array
+
 	return 0;
 }
